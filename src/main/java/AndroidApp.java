a0d2d787e1831424137052839ddae2ccab047852
PrakharLT <141761576+PrakharLT@users.noreply.github.com>
2024-03-02 01:42:13 +0530

    Update AndroidApp.java

--- src/main/java/AndroidApp.java
+++ src/main/java/AndroidApp.java
@@ -16,6 +16,7 @@ public class AndroidApp {
     String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "accessKey" : System.getenv("LT_ACCESS_KEY"); //Add accessKey here
     String app_id = System.getenv("LT_APP_ID") == null ? "lt://proverbial-android" : System.getenv("LT_APP_ID");      //Enter your LambdaTest App ID at the place of lt://proverbial-android
     String grid_url = System.getenv("LT_GRID_URL") == null ? "mobile-hub.lambdatest.com" : System.getenv("LT_GRID_URL");
+    String buildName = System.getenv("BuildName") == null ? "GenericName" : System.getenv("BuildName"); //Add buildName here
 
     AppiumDriver driver;
 
@@ -24,7 +25,7 @@ public class AndroidApp {
     public void AndroidApp1(String device, String version, String platform) {
         try {
             DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("build", "Java TestNG");
+            capabilities.setCapability("build", buildName);
             capabilities.setCapability("name", platform + " " + device + " " + version);
             capabilities.setCapability("deviceName", device);
             capabilities.setCapability("platformVersion", version);
